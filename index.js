c2e17db5aa81c25419b6e93dbedfa7db200cc89a
Aatu Mikkonen <t5miaa00@students.oamk.fi>
2017-03-20 09:41:31 +0200

    Added more dependancies to the node server. Created a basic "Hello, world!" route also

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+/* BORDER PASS SERVER
+
+   Created for Software Development Project 2
+*/
+// Included files
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+
+// Setting up the port
+app.set('port', (process.env.PORT || 80));
+
+// Starting to use modules
+app.use(cors());
+app.use(bodyParser.json());
+
+// Base route
+app.get('/', function(req, res) {
+   
+   res.send("Hello, world!");
+});
