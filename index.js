5423c4014c3abb94db1ac4c45b85feddcbc93116
damsu <damorizur@gmail.com>
2017-03-20 17:04:20 +0200

    Insert dummy documents to collection + GET documents at /crossings

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-
+var crossings_collection;
 var url = 'mongodb://localhost:27017/borderpass';
 
 // Setting up the port
@@ -25,37 +25,42 @@ app.get('/', function(req, res) {
    res.send('Hello, world!');
 });
 
+// Connect to MongoDB and create a Crossings Collection
 MongoClient.connect(url, function (err, db) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
     console.log('Connection established to', url);
 
-    db.createCollection("cars", function(err, collection){
-	   if (err) throw err;
-
-	   	console.log("Created Cars Collection");
-	 		//console.log(collection);
-	});
-
-    var cars_collection = db.collection('cars');
-
-	var car1 = {owner_id: 1, vehicle_type: 'A', car_plate: 'XIH-222', car_manufacturer: 'Renault', car_model: 'Logan'};
-	var car2 = {owner_id: 2, vehicle_type: 'A', car_plate: 'XIH-222', car_manufacturer: 'Renault', car_model: 'Logan'};
-	var car3 = {owner_id: 3, vehicle_type: 'A', car_plate: 'XIH-222', car_manufacturer: 'Renault', car_model: 'Logan'}
-
-    cars_collection.insert([car1, car2, car3], function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Inserted %d documents into the "cars" collection. The documents inserted with "_id" are:', result.length, result);
-        cars_collection.find();
-      }
-      //Close connection
-      db.close();
+    db.createCollection("crossings", function(err, collection){
+      if (err) throw err;
+      console.log("Created Crossings Collection");
     });
-    /**/
+
+    crossings_collection = db.collection('crossings');
   }
+});
+
+//Create GET route as a test to GET Crossings data from MongoDB
+app.get('/crossings', function(req, res) {
+
+  //Dummy documents for Crossings collection.
+  var crossing = {test: 'this', or: 'this', and: 'that'};
+  var crossing2 = {test: 'thisssss', or: 'thisssss', and: 'thattttt'};
+
+  //Insert them into the collection
+  crossings_collection.insert([crossing,crossing2], function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Successfully inserted : ', result);
+    }
+  })
+
+  //Fetch all documents on Crossings collection + return as Json
+  crossings_collection.find().toArray(function(err, docs) {
+    return res.json(docs);
+  }); 
 });
 
 // Listening to a port
