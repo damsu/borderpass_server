6a79024f6b6677eae10c53f6ce026f4f1da9c23f
t5miaa00 <t5miaa00@students.oamk.fi>
2017-03-28 10:54:00 +0300

    created a connection callback function which is now more or less deprecreated.

--- models/Db.js
+++ models/Db.js
@@ -1,5 +1,22 @@
-var MongoClient = require('mongodb').MongoClient;
-var uristring =
+// vim: set ts=2 sw=2 softtabstop=2:
+
+var mongo = require('mongodb').MongoClient;
+const MONGO_URL = (process.env.MONGODB_URI) ? process.env.MONGODB_URI : 'mongodb://localhost:27017/borderpass';
+
+exports.connect = function(callback) {
+
+  var dbref;
+  mongo.connect(MONGO_URL, function(err, db) {
+
+    if (err) {
+
+      console.log('MongoDB error happened: ' + err);
+    }
+    dbref = db;
+  });
+  callback(dbref);
+}
+/*var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
 	'mongodb://localhost:27017/borderpass';
@@ -18,4 +35,4 @@ exports.DbConnect = function() {
 		}
 	});
 	return useDb;
-}
+}*/
