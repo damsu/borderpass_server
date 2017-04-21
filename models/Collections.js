950b47b5ad57857416020da6c9a537617c84c770
t5miaa00 <t5miaa00@students.oamk.fi>
2017-04-21 21:27:41 +0300

    Removed redundant data from Border crossing items

--- models/Collections.js
+++ models/Collections.js
@@ -24,56 +24,49 @@ exports.addCrossings = function(dbref, callback) {
 		from_city: 'Nuijamaa',		to_city: 'Brusnichnoe', address: 'Nuijamaa',
 		from_flag_url: 'fi.png',	to_flag_url: 'ru.png',
 		short_name_from: 'fi',		short_name_to: 'ru',
-		service_provider: 'Finnish Transport Agency',
-		timeslots: []
+		service_provider: 'Finnish Transport Agency'
 	};
 	var crossings_3 = {
 		from_country: 'Finland',	to_country: 'Russia',
 		from_city: 'Niirala',		to_city: 'Wärtsilä',		address: 'Niirala',
 		from_flag_url: 'fi.png',	to_flag_url: 'ru.png',
 		short_name_from: 'fi',		short_name_to: 'ru',
-		service_provider: 'Finnish Transport Agency',
-		timeslots: []
+		service_provider: 'Finnish Transport Agency'
 	};
 	var crossings_4 = {
 		from_country: 'Russia',		to_country: 'Finland',
 		from_city: 'Svetogorsk',	to_city: 'Imatra',		address: 'Svetogorsk',
 		from_flag_url: 'ru.png',	to_flag_url: 'fi.png',
 		short_name_from: 'ru',		short_name_to: 'fi',
-		service_provider: 'Border Service of the Federal Security Service of the Russian Federation',
-		timeslots: []
+		service_provider: 'Border Service of the Federal Security Service of the Russian Federation'
 	};
 	var crossings_5 = {
 		from_country: 'Russia',		to_country: 'Finland',
 		from_city: 'Brusnichnoe',	to_city: 'Nuijamaa',		address: 'Brusnichnoe',
 		from_flag_url: 'ru.png',	to_flag_url: 'fi.png',
 		short_name_from: 'ru',		short_name_to: 'fi',
-		service_provider: 'Border Service of the Federal Security Service of the Russian Federation',
-		timeslots: []
+		service_provider: 'Border Service of the Federal Security Service of the Russian Federation'
 	};
 	var crossings_6 = {
 		from_country: 'Russia',		to_country: 'Finland',
 		from_city: 'Wärtsilä',		to_city: 'Niirala',		address: 'Wärtsilä',
 		from_flag_url: 'ru.png',	to_flag_url: 'fi.png',
 		short_name_from: 'ru',		short_name_to: 'fi',
-		service_provider: 'Border Service of the Federal Security Service of the Russian Federation',
-		timeslots: []
+		service_provider: 'Border Service of the Federal Security Service of the Russian Federation'
 	};
 	var crossings_7 = {
 		from_country: 'Russia',		to_country: 'Norway',
 		from_city: 'Borisoglebsky',to_city: 'Storskog',		address: 'Borisoglebsky',
 		from_flag_url: 'ru.png',	to_flag_url: 'no.png',
 		short_name_from: 'ru',		short_name_to: 'no',
-		service_provider: 'Border Service of the Federal Security Service of the Russian Federation',
-		timeslots: []
+		service_provider: 'Border Service of the Federal Security Service of the Russian Federation'
 	};
 	var crossings_8 = {
 		from_country: 'Norway',		to_country: 'Russia',
 		from_city: 'Storskog',     to_city: 'Borisoglebsky',address: 'Storskog',
 		from_flag_url: 'no.png',	to_flag_url: 'ru.png',
 		short_name_from: 'no',		short_name_to: 'ru',
-		service_provider: 'Norwegian Customs Se',
-		timeslots: []
+		service_provider: 'Norwegian Customs Se'
 	};
 
 	//Replace them into collection
